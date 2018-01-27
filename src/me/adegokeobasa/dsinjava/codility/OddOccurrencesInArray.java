8251c3c5825014ed823dcaa26c69f970a332ede8
Adegoke Obasa <adegokeobasa@gmail.com>
2018-01-27 14:31:48 +0100

    modify: simplify solution to OddOccurencesInArray - Codility Lesson 2

--- src/me/adegokeobasa/dsinjava/codility/OddOccurrencesInArray.java
+++ src/me/adegokeobasa/dsinjava/codility/OddOccurrencesInArray.java
@@ -5,16 +5,13 @@ package me.adegokeobasa.dsinjava.codility;
  */
 public class OddOccurrencesInArray {
     public static void main(String[] args) {
-        System.out.println(new OddOccurrencesInArray().solution(new int[]{1,2,2,4,4, 1, 9}));
+        System.out.println(new OddOccurrencesInArray().solution(new int[]{1, 2, 2, 4, 4, 1, 9}));
     }
+
     public int solution(int[] A) {
         int result = 0;
         for (int i = 0; i < A.length; i++) {
-            if(i == 0) {
-                result = A[i];
-            } else {
-                result ^= A[i];
-            }
+            result ^= A[i];
         }
         return result;
     }
