8735d742a10757b1bbcce315024e596a1ebd15a7
Adegoke Obasa <adegokeobasa@gmail.com>
2018-01-28 12:12:08 +0100

    add: solution for FrogJmp - Codility - Lesson 3

---
+++ src/me/adegokeobasa/dsinjava/codility/lessonthree/FrogJmp.java
@@ -0,0 +1,19 @@
+package me.adegokeobasa.dsinjava.codility.lessonthree;
+
+/**
+ * Created by epapa on 28/01/2018.
+ */
+public class FrogJmp {
+
+    public static void main(String[] args) {
+        System.out.println(new FrogJmp().solution(10, 85, 30));
+    }
+
+    public int solution(int X, int Y, int D) {
+        int minimalSteps = (Y - X) / D;
+        if((Y-X) % D > 0) {
+            minimalSteps++;
+        }
+        return minimalSteps;
+    }
+}
