00e9e26b61a3c32b0afddd70b0e881fb6661eb8d
Adegoke Obasa <adegokeobasa@gmail.com>
2018-02-09 12:37:21 +0100

    add: solution for Count and Say - Leetcode - https://leetcode.com/problems/count-and-say/description/

---
+++ src/me/adegokeobasa/dsinjava/leetcode/CountAndSay.java
@@ -0,0 +1,42 @@
+package me.adegokeobasa.dsinjava.leetcode;
+
+/**
+ * Created by epapa on 09/02/2018.
+ */
+public class CountAndSay {
+
+    public static void main(String[] args) {
+        System.out.println(new CountAndSay().countAndSay(2));
+    }
+
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+
+        int runs = 0;
+        String result = "1";
+        while (runs < n - 1) {
+            String runResult = "";
+            runs++;
+            char curr = '.';
+            int charCount = 0;
+            char[] chars = result.toCharArray();
+            for (int i = 0; i < result.length(); i++) {
+                if (curr != chars[i]) {
+                    if (curr != '.') {
+                        runResult += String.valueOf(charCount) + curr;
+                    }
+                    curr = chars[i];
+                    charCount = 1;
+                } else {
+                    charCount++;
+                }
+
+                if (i == result.length() - 1) {
+                    runResult += String.valueOf(charCount) + curr;
+                }
+            }
+            result = runResult;
+        }
+        return result;
+    }
+}
