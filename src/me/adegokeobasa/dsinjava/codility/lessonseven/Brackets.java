ba9f164bff765ff9bef7920ab7d2bf68617ab0fb
Adegoke Obasa <adegokeobasa@gmail.com>
2018-03-20 14:15:56 +0100

    add: solution to Brackets - lesson seven - codility

---
+++ src/me/adegokeobasa/dsinjava/codility/lessonseven/Brackets.java
@@ -0,0 +1,32 @@
+package me.adegokeobasa.dsinjava.codility.lessonseven;
+
+import java.util.Stack;
+
+/**
+ * Created by epapa on 20/03/2018.
+ */
+public class Brackets {
+
+    public static void main(String[] args) {
+        System.out.println(new Brackets().solution("{[()()]}"));
+        System.out.println(new Brackets().solution("{{{"));
+    }
+
+    public int solution(String S) {
+        Stack<Character> characterStack = new Stack<>();
+        if (S.isEmpty()) return 1;
+
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                characterStack.push(c);
+            } else {
+                if (characterStack.isEmpty()) return 0;
+                if (c == ')' && characterStack.pop() != '(') return 0;
+                if (c == '}' && characterStack.pop() != '{') return 0;
+                if (c == ']' && characterStack.pop() != '[') return 0;
+            }
+        }
+        return characterStack.isEmpty() ? 1 : 0;
+    }
+}
